fcbae1e07157caee81095c2440fa98e2e9ea5bdd
suraj.kurde <svkurde03@gmail.com>
2022-12-05 10:19:33 +0530

    array and linkedlist examples

--- src/ExamplesDP.java
+++ src/ExamplesDP.java
@@ -1,4 +1,7 @@
-import org.w3c.dom.html.HTMLLIElement;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class ExamplesDP {
     //Given array arr[], minimum(max+min) for subarray
@@ -156,19 +159,198 @@ public class ExamplesDP {
     return longS;
     }
 
+//triangle sum of array
+//0 //          48
+//1 //        20 28
+//2 = n-3//      8  12  16
+//3 = n-2//    3   5   7   9
+//4 = n-1//  1  2  3   4    5
+
+
+    public static void triangleSum(int arr[]){
+        int n=arr.length;
+        if(n==1) {
+            System.out.println("Array size is 1, Triangle sum is : " + arr[0]);
+            return;
+        }
+        int sumArr[][] = new int[n][n];
+
+        //copy elements from given array in solution/answer matrix
+        for(int i=0;i<n;i++)
+            sumArr[n-1][i] = arr[i];
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                sumArr[i][j] = sumArr[i+1][j] + sumArr[i+1][j+1];
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(sumArr[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("Triangle sum is : " + sumArr[0][0]);
+    }
+
+
+    //given array
+    //  1 2 3 4 5
+    //   3 5 7 9
+    //    8 2 6
+    //     0 8
+    //      8
+//       sum%10
+
+    static void modSum(int arr[]){
+        int n = arr.length;
+        if(n==1){
+            System.out.println("Array is of length 1, Mod sum is : " + arr[0]);
+            return;
+        }
+        int sumArr[][] = new int[n][n];
+        int i,j;
+        for(i=0;i<n;i++){
+            sumArr[0][i] = arr[i];
+        }
+
+        for(i=1;i<n;i++){
+            for(j=0;j<n-i;j++){
+                sumArr[i][j] = (sumArr[i-1][j] + sumArr[i-1][j+1])%10;
+            }
+        }
+        System.out.println("Mod Sum is : " + sumArr[n-1][0]);
+    }
+
+
+    // given an array = {8,4,6,2,1,0,5,3}, containing numbers [0,n], find missing number
+    // O(n)
+    static int missingNumber(int []arr){
+        int ind=0;
+        //O(n^2)
+//        for(int i=0;i<=arr.length;i++){
+//            boolean found=false;
+//            for(int j=0;j<arr.length;j++){
+//                if(i==arr[j]){
+//                    found=true;
+//                    break;
+//                }
+//            }
+//            if(!found){
+//                System.out.println("missing number is : " + i);
+//                ind=i;
+//            }
+//             }
+//        return ind;
+        int n=arr.length;
+        int sum= (n * (n+1))/2;
+        int eleSum=0;
+        for(int e:arr){
+            //eleSum+=e;
+            sum-=e;
+        }
+
+//        return sum-eleSum;
+        return sum;
+    }
+
+//    {0,1,2,3,5,6,7}
+//    [0,1,2,3,4,5,6]
+    static int missingInSortedArr(int []arr){
+        int num=0,i=0;
+        //linear search
+//        while(i<arr.length){
+//            if(i!=arr[i]) {
+//                return i;
+//            }
+//            i++;
+//        }
+//        return i;
+
+
+        //xor
+        // 2^1^2 = 1
+        while(i<arr.length){
+            num = num ^ i ^ arr[i];
+            i++;
+        }
+        return num^i;
+    }
+
+    //sqrt(n) - O(log n)
+    static int sqrtN(int n){
+        long left=0,right=n;
+        while(left<right){
+            long mid = left +((long)right-left+1)/2;
+            if((mid*mid)<=(long)n){
+                left = mid;
+            }else{
+                right = mid-1;
+            }
+        }
+        return (int)left;
+    }
+
+    // Given an array = [] , number = n,
+    //find first and last occurence of number n
+//    {2,4,5,7,7,7,7,7,8}
+//    n = 7
+//    first = 4
+//    last = 7
+//    O(log n)
+
+    //try
+
+
+    // arrayList and LinkedList
+    // add arraylist to Linkedlist
+
+    static void appendArraylist(){
+        ArrayList<Integer> aList = new ArrayList<>();
+        LinkedList<Integer> llist = new LinkedList<>();
+
+        aList.add(7);
+        aList.add(8);
+        aList.add(9);
+        aList.add(10);
+
+        llist.add(1);
+        llist.add(2);
+        llist.add(3);
+        llist.add(4);
+
+        for(Integer i:aList){
+            llist.add(i);
+        }
+        System.out.println(llist);
+    }
+
 
 
 
     public static void main(String[] args) {
-        int arr[]={5,2,6,8,19,1,2,3,4,5,6,7,8,9,10,11,12};
+        int arr[]={0,1,2,3,4,6,7};
         
 //        System.out.println("Minimum (max+min) is : " + findMinSum(arr));
 //        System.out.println("Closest product pair solution for given array : " );
 //        closePair(arr,42);
 
-        System.out.println("Longest Increasing subsequence is of length : " + findLIS(arr,arr.length));
+//        System.out.println("Longest Increasing subsequence is of length : " + findLIS(arr,arr.length));
 
+//        System.out.println(" Finding triangle sum for given array :");
+//        triangleSum(arr);
 
+//        System.out.println(" MOd sum for given array :");
+//        modSum(arr);
+
+//        System.out.println(missingNumber(arr));
+//        System.out.println(missingInSortedArr(arr));
+
+//        int x=30;
+//        System.out.println(" Square root of " + x + " is : " + sqrtN(x));
+
+        appendArraylist();
     }
 }
 
